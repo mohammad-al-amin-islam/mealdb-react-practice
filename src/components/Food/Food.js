8a2c63a86181d16e02d05b44298de6b554d7ef45
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-03-24 12:00:05 +0600

    food area added

--- src/components/Food/Food.js
+++ src/components/Food/Food.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Food.css'
 
 const Food = (props) => {
     console.log(props);
+    const { strMealThumb, strMeal, strYoutube } = props.food
     return (
-        <div>
-            <h1>This is food</h1>
+        <div className='food-area'>
+            <div className='food'>
+                <img src={strMealThumb} alt="" />
+                <div className="food-details">
+                    <h3>Name:{strMeal}</h3>
+                    <p>{strYoutube}</p>
+                </div>
+                <button className='btn-cart'>Add To Cart</button>
+
+            </div>
         </div>
     );
 };
