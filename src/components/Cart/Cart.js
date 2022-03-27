ac9cb4b0a4e4effccda997035260829f413febe0
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-03-27 21:45:40 +0600

    local storage added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 const Cart = ({ cart }) => {
+    let count = 0;
+    for (const meal of cart) {
+        count = meal.quantity + count;
+    }
 
     return (
-        <ol>
-            {
-                cart.map(food => <li>{food.strMeal}</li>)
-            }
-        </ol>
+        <div>
+            <h2>Added Foods:{count}</h2>
+            <ol>
+                {
+                    cart.map(food => <li>{food.strMeal}</li>)
+                }
+            </ol>
+        </div>
+
     );
 };
 
--- src/components/FoodsSelect/FoodsSelect.js
+++ src/components/FoodsSelect/FoodsSelect.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, storedCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Food from '../Food/Food';
 import './FoodsSelect.css'
@@ -11,9 +12,35 @@ const FoodsSelect = () => {
             .then(res => res.json())
             .then(data => setFoods(data.meals));
     }, []);
-    const foodButtonHandle = (food) => {
-        const newCart = [...cart, food];
+    useEffect(() => {
+        const added = storedCart();
+        let newCart = [];
+        for (const id in added) {
+            const exits = foods.find(food => food.idMeal === id);
+            if (exits) {
+                const quantity = added[id];
+                exits.quantity = quantity;
+                newCart.push(exits);
+            }
+        }
         setCart(newCart);
+
+    }, [foods])
+    const foodButtonHandle = (food) => {
+        let newCart = [];
+        const exists = cart.find(m => m.idMeal === food.idMeal);
+        if (exists) {
+            const rest = cart.filter(m => m.idMeal !== food.idMeal);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+        else {
+            food.quantity = 1;
+            newCart = [...cart, food];
+        }
+        // const newCart = [...cart, food];
+        setCart(newCart);
+        addToDb(food.idMeal);
     }
     return (
         <div className='foods-select'>
@@ -27,9 +54,7 @@ const FoodsSelect = () => {
                 }
             </div>
             <div className="food-cart">
-                <h2>Added Foods</h2>
                 <Cart cart={cart}></Cart>
-
             </div>
         </div>
     );
